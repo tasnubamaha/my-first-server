9ac698b22fc7a7f9e494fc92d0c2f5cfe6046774
tasnubamaha <tasnubamaha267@gmail.com>
2023-04-12 21:21:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+app.get('/',(req, res) =>{
+    res.send('hello from my first server');
+})
+
+app.get('/data',(req, res) =>{
+    res.send('more data coming soon');
+})
+
+
+app.listen(port, () => {
+    console.log(`my first server is running on port: ${port}`)
+})
